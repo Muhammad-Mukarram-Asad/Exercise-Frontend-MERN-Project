4e62cd6949391db395ae3191493c1fc017f71086
Mukarram Asad <76425569+Muhammad-Mukarram-Asad@users.noreply.github.com>
2025-07-23 23:41:12 +0500

    Update index.js

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,4 +1,8 @@
 setTimeout(()=>{
   console.log("You Will Win "):
-});
+}, [1500]);
+
+setTimeout(() => {
+  console.log("Running towards winning...");
+})
 console.log("Before you win, you should have to start");
