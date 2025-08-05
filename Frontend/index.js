550336b582f047b9c84dffc385267c529ac13116
Mukarram Asad <76425569+Muhammad-Mukarram-Asad@users.noreply.github.com>
2025-08-06 00:02:09 +0500

    Update index.js

--- Frontend/index.js
+++ Frontend/index.js
@@ -24,3 +24,41 @@ useEffect(() => {
 }, [displayData]);
 
 // Below is the css for showing outer layer infront of the screen
+
+.calendar {
+  z-index: -1;
+  position: fixed;
+  background: rgba(57, 122, 133, 0.5);
+
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  opacity: 0;
+  transition: opacity 125ms ease-in-out;
+}
+
+.calendar > div {
+  background-color: rgba(57, 122, 133, 0.5);
+  transform: translateY(100%);
+  background-color: white;
+  width: 100%;
+  transition: transform 250ms ease-out;
+
+  padding-inline: 20px;
+  padding-top: 20px;
+  border-top-left-radius: 3rem;
+  border-top-right-radius: 3rem;
+}
+
+.active {
+  opacity: 1;
+  z-index: 1;
+  transition: opacity 125ms ease-in-out;
+}
+
+.active > div {
+  transition: all 250ms ease;
+  transform: translateY(0%);
+}
